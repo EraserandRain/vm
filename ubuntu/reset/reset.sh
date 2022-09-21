ef64c658ea7f61d36903b33ba1c5c43faa984a90
EraserandRain <1349291258@qq.com>
2022-09-21 13:24:07 +0800

    feat: vm start settings, add gitignore file

--- ubuntu/reset/reset.sh
+++ ubuntu/reset/reset.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 export LC_ALL=C
 
-# git clone 
+Device=`hostname -s`
 cd ~ && git clone https://github.com/eraserandrain/install.git
+source ~/install/include/common.sh
 
+# Install
+disable_needrestart
+set_apt_mirror
+install_ubuntu_docker
+install_docker_compose
+install_python3_env
+install_cpp_env
+install_node_env
+install_zsh
+# Load
+load_path
+load_zsh
+load_nvm
+load_alias
+reload_zsh $Device
