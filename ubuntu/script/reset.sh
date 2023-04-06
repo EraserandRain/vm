efd9c3016f3365fe36437d52e5bc88742fcdba2a
EraserandRain <1349291258@qq.com>
2023-04-06 18:41:29 +0800

    test: testing...some error need be fixed

--- ubuntu/script/reset.sh
+++ ubuntu/script/reset.sh
@@ -2,9 +2,5 @@
 export LC_ALL=C
 
 Device=$(hostname -s)
-cd $HOME && git clone https://github.com/eraserandrain/env-manager.git
-cd $HOME/env-manager
-
-# Other Settings
-bash init.sh
-./update_local_zsh.sh -c
+cd $HOME 
+echo "test" > testfile
